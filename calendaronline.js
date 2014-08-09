bcf604ee0e3ced41fc67ba67863316d5370c8398
lukehillonline <lukehillonline@gmail.com>
2014-08-09 21:03:40 +0100

    refactoring an funcationalising

--- calendaronline.js
+++ calendaronline.js
@@ -8,7 +8,6 @@ $(function() {
 		var month = moment().month();
 		var year = moment().year();
 		var daysInMonth = moment().daysInMonth();
-
 		var target = $(options.container);
 
 		if(options.months) {
@@ -21,6 +20,7 @@ $(function() {
 			var i = 0;
 			var current_month = month;
 			var current_year = year;
+			var content = "";
 
 			// loop through number of months to display (starting from current month)
 			while(i < numMonths) {
@@ -28,26 +28,9 @@ $(function() {
 				var curr = moment([current_year, current_month]);
 				var begin = curr.clone().startOf('isoWeek').isoWeekday(1);
 
-				var content = '<p>'+curr.format('MMMM YYYY')+'</p>'
-							+ '<table border=1><thead><tr><td>Mon</td><td>Tues</td><td>Wed</td><td>Thur</td><td>Fri</td><td>Sat</td><td>Sun</td></thead><tbody>';
-
-				for (var w=0; w < 5; w++) {
-					content += '<tr>';
-					// loop through days of week
-					for (var d=0; d<7; d++) {
-						content += '<td>';
-						if (begin.month() == current_month) {
-							content += begin.format('DD MM YYYY');
-						}
-						content += '</td>';
-						begin.add('d', 1);
-					}
-					content += '</tr>';
-				}
-
-				content += '</tr></tbody></table>';
-
-				target.append(content);
+				content += addTitle(curr);
+				content += addDaysOfWeek();
+				content += addDaysOfMonth(begin, current_month);
 
 				if (current_month == 11) {
 					++current_year;
@@ -58,12 +41,46 @@ $(function() {
 
 				++i;
 			}
+
+			displayMonths(content);
+		}
+
+		function addTitle(curr) {
+			return '<p>'+curr.format('MMMM YYYY')+'</p>';
+		}
+
+		function addDaysOfWeek() {
+			return '<table border=1><thead><tr><td>Mon</td><td>Tues</td><td>Wed</td><td>Thur</td><td>Fri</td><td>Sat</td><td>Sun</td></thead>';
+		}
+
+		function addDaysOfMonth(begin, current_month) {
+			var monthContent = "<tbody>"
+			for (var w=0; w < 5; w++) {
+				monthContent += '<tr>';
+				// loop through days of week
+				for (var d=0; d<7; d++) {
+					monthContent += '<td>';
+					if (begin.month() == current_month) {
+						monthContent += begin.format('DD MM YYYY');
+					}
+					monthContent += '</td>';
+					begin.add('d', 1);
+				}
+				monthContent += '</tr>';
+			}
+			monthContent += "</tr></tbody></table>";
+
+			return monthContent;
+		}
+
+		function displayMonths(content) {
+			target.html(content);
 		}
 	}
 
 	calendaronline({
-		months: 6,
-		container: '#calendar'
-	})
+		container: '#calendar',
+		months: 6
+	});
 
 });
